03df4f8376947b77bd7206c19e5192e84f1424d7
Liam Young <liam.young@canonical.com>
2012-09-26 20:10:40 +0100

    Moved common jetty start code into a seperate function

---
+++ hooks/common.sh
@@ -0,0 +1,18 @@
+jetty_start() {
+    juju-log "Starting solr-jetty"
+    /etc/init.d/jetty start
+    # Wait for solr-jetty to start responding
+    curl_rc=-1
+    counter=0
+    while [[ $curl_rc -ne 0 ]]; do
+        juju-log "Waiting for solr-jetty to respond ($counter)"
+        curl --silent --max-time 5 "http://localhost:8080/solr/select?q=test" 2>&1 > /dev/null
+        curl_rc=$?
+        if [[ $counter -gt 5 ]]; then
+            juju-log "solr-jetty not responding"
+    	    exit 1
+        fi
+        sleep 2
+        let counter++
+    done
+}
